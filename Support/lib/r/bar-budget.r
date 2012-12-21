df5e06c83b47d2e9c5249b6585ec0e1adfabf2ef
Lifepillar <github@lifepillar.com>
2012-12-21 17:47:55 +0100

    Resize budget chart.

--- Support/lib/r/bar-budget.r
+++ Support/lib/r/bar-budget.r
@@ -2,11 +2,11 @@
 p <- ggplot(ledger_data, aes(partial_account)) +
             geom_bar(aes(y = actual), width = .75, fill = "grey55") +
             geom_errorbar(aes(ymin = budgeted, ymax = budgeted), size = 1 ) +
-            facet_wrap(~ partial_account, ncol = 4, scales = "free") +
+            facet_wrap(~ partial_account, ncol = 6, scales = "free") +
             geom_text(aes(y=1, label = paste(round(ledger_data$remaining), " (", round(ledger_data$used), "%)", sep=""),
-                      vjust = -0.6, size = 4)) +
+                      vjust = -0.6, size = 6)) +
             xlab("") +
             ylab(commodity)
 
-suppressMessages(ggsave(file="bar-budget.svg", plot = p, width = 12, height = 4 * (length(ledger_data$account) / 4)))
+suppressMessages(ggsave(file="bar-budget.svg", plot = p, width = 14, height = 2 * (length(ledger_data$account) / 4)))
 
